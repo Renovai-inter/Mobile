0440a0848c810bfa7f63589842390372d66a4098
Gabriel Andozia <gabrielparma2016@gmail.com>
2026-08-18 10:19:59 -0300

    Adicionando ligação entre escolha cadastro e cadastro empresa

--- app/src/main/java/com/gabriel/renovai/view/EscolheActivity.java
+++ app/src/main/java/com/gabriel/renovai/view/EscolheActivity.java
@@ -1,14 +1,14 @@
 package com.gabriel.renovai.view;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.gabriel.renovai.R;
 
@@ -28,5 +28,22 @@ public class EscolheActivity extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.parseColor("#882B4E"));
 
         setContentView(R.layout.activity_escolhe);
+
+        // Implementando os elementos
+        ImageView btnVoltar = findViewById(R.id.btnVoltar);
+        View empresaButton = findViewById(R.id.empresaButton);
+        View cooperativaButton = findViewById(R.id.cooperativaButton);
+
+        // Ir para o cadastro de empresa
+        empresaButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, CadastroEmpresaActivity.class);
+            startActivity(intent);
+        });
+
+        // Voltar para tela de login
+        btnVoltar.setOnClickListener(v -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
