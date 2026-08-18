9e8581918c855469a99854690fae1b6e1a48796f
Gabriel Andozia <gabrielparma2016@gmail.com>
2026-08-18 09:22:52 -0300

    Adicionando a status bar color na escolhe activity

--- app/src/main/java/com/gabriel/renovai/view/EscolheActivity.java
+++ app/src/main/java/com/gabriel/renovai/view/EscolheActivity.java
@@ -1,11 +1,13 @@
 package com.gabriel.renovai.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.gabriel.renovai.R;
@@ -15,12 +17,16 @@ public class EscolheActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
+        WindowCompat.getInsetsController(
+                getWindow(),
+                getWindow().getDecorView()
+        ).setAppearanceLightStatusBars(false);
+
+        getWindow().setStatusBarColor(Color.parseColor("#882B4E"));
+
         setContentView(R.layout.activity_escolhe);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
